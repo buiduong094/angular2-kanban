f503a4d96f0f08697b06451b0282d65322c276b1
buiduong094 <bui.van.duong@framgia.com>
2017-11-01 17:44:27 +0700

    Second Commit - Init angular

---
+++ client/app/ts/app.ts
@@ -0,0 +1,51 @@
+/*
+ * Angular Imports
+ */
+import {
+  NgModule
+} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+
+/*
+ * Components
+ */
+import {KabanApp} from './app.component';
+import {HomeComponent} from './components/HomeComponent';
+import {AboutComponent} from './components/AboutComponent';
+
+/*
+ * Webpack
+ */
+require('css/styles.css');
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  declarations: [
+    KabanApp,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  bootstrap: [ KabanApp ],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+})
+class KabanAppModule {}
+
+platformBrowserDynamic().bootstrapModule(KabanAppModule)
+  .catch((err: any) => console.error(err));
